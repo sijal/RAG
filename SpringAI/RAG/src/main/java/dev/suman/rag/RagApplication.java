868b7617fbcdf1af74a5d2bd6f8e9d0a286fd730
Suman <sumann7676@icloud.com>
2025-05-27 22:36:02 +0530

    rag main file

---
+++ SpringAI/RAG/src/main/java/dev/suman/rag/RagApplication.java
@@ -0,0 +1,13 @@
+package dev.suman.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagApplication.class, args);
+	}
+
+}
